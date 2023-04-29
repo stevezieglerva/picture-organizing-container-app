7bf090c0e027fef211fa2423fb27ffc2c15d2f6a
Steve Ziegler <stephen.v.ziegler@gmail.com>
2023-04-28 20:21:31 -0400

    about to delete picture file depo since picture is abstract enought

--- sam-app/common_layer/python/infrastructure/repository/PictureFileRepo.py
+++ sam-app/common_layer/python/infrastructure/repository/PictureFileRepo.py
@@ -8,10 +8,17 @@ class GettingPictureFiles(ABC):
     def get_picture_file(self, source: str) -> Picture:
         raise NotImplemented
 
+    def save_picture_file(self, source: str, picture: Picture) -> None:
+        raise NotImplemented
+
 
 class PictureFileRepo(GettingPictureFiles):
     def __init__(self, image_io: ImageIO):
         self._image_io = image_io
+
+    def get_picture_file(self, source: str) -> Picture:
+        picture = Picture(source, self._image_io)
+        return picture
 
     def get_picture_file(self, source: str) -> Picture:
         picture = Picture(source, self._image_io)
